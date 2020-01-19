aee584202fe2e1ff6ab398465fb7e8deda230707
jpoist97 <jpoist97@gmail.com>
2020-01-18 20:49:29 -0800

    Fixed stylesheet linking and added Textbook model and modelform

--- BrokeBoyBooks/BrokeBoyBooks/urls.py
+++ BrokeBoyBooks/BrokeBoyBooks/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('textbook_search/', include('textbook_search.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
