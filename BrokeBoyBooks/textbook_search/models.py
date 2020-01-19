12a944cc96d163f4a7e306a97686d524095f57e9
jpoist97 <jpoist97@gmail.com>
2020-01-19 11:19:16 -0800

    Populated database with all textbook entries from excel file

--- BrokeBoyBooks/textbook_search/models.py
+++ BrokeBoyBooks/textbook_search/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Textbook(models.Model):
+    #auto_increment_id = models.AutoField(editable=False, primary_key=True)
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=75)
     edition = models.IntegerField()
